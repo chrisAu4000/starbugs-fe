4c555fe9a7bb27a08e4a18c19b0b6258d0c01a2c
Christian Auer <s0535956@htw-berlin.de>
2016-10-21 18:39:55 +0200

    styled navigation in ugly banana yellow :D

--- src/components/navigation/navigation-view.js
+++ src/components/navigation/navigation-view.js
@@ -2,7 +2,7 @@ import {div, ul, li, nav, a} from '@motorcycle/dom'
 
 const view = (prop$) => {
   return prop$.map(props =>
-    nav('.navigation', [
+    nav([
       ul('.navigation-list', props.map(p =>
         li('.navigation-list-item', [
           a('.navigation-list-item-link', {attrs: {data: p.href}}, [p.title])
--- src/main.js
+++ src/main.js
@@ -7,26 +7,23 @@ import config from './config'
 import Home from './pages/home/home-index'
 import Slideshow from './pages/slideshow/slideshow-index'
 import Init from './pages/init/init-index'
+import Login from './pages/login/login-index.js'
 
 const navProps = [
-  {href: '/', title: 'Home', handler: Home}
+  {href: '/', title: 'Starbugs', handler: Home}
 , {href: '/slideshow', title: 'Slideshow', handler: Slideshow}
 , {href: '/initAdmin', title: 'Admin', handler: Init}
+, {href: '/login', title: 'Login', handler: Login}
 ]
 
 const main = (sources) => {
-  const request$ = just({url: config.url.init, method: 'POST'})
   const page       = Router(sources, navProps)
   const navigation = Navigation(sources, navProps)
-  const initialRoute$ = sources.HTTP
-    .filter(res$ => res$.request.url === config.url.init)
-    .join()
-    .map(res => res.usercount)
-    .map(ucount => ucount <= 0 ? '/initAdmin' : '/')
+  const initialRoute$ = just('/login');
 
   const view$      = view({navigation$: navigation.DOM, page$: page.DOM})
   const route$     = mergeArray([navigation.router, page.router, initialRoute$])
-  const http$      = mergeArray([request$, page.HTTP])
+  const http$      = mergeArray([page.HTTP])
   return {
     DOM: view$,
     HTTP: http$,
