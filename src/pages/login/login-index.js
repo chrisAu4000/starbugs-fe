2f674a2eb10aa8889cdde400cdab48335a15a73d
Christian Auer <s0535956@htw-berlin.de>
2016-10-25 01:02:26 +0200

    added login page

--- src/pages/login/login-index.js
+++ src/pages/login/login-index.js
@@ -2,7 +2,8 @@ import {delay, just} from 'most'
 import model from './login-model'
 import view from './login-view'
 import intent from './login-intent'
-
+import HTTPForm from '../../components/http-form/http-form-index'
+import config from '../../config'
 /*
 States:
 initial: {disabled: false, errors: [], success: [], spinner: false}
@@ -11,29 +12,42 @@ Error: {disabled: false, errors: [''...], success: [], spinner: false}
 success: {disabled: true, errors: [], success: String, spinner: false}
 */
 const Login = (sources) => {
-  const initialState$ = just({
-    disabled: false,
-    errors: [],
-    success: [],
-    spinner: false
-  });
+  const formProp$ = just({
+    inputs: [
+      {as: 'username', prop$: just({disabled: false, placeholder: 'Username', type: 'text', value: ''})},
+      {as: 'password', prop$: just({disabled: false, placeholder: 'Password', type: 'password', value: ''})}
+    ],
+    messageBox: {
+      prop$: just([])
+    },
+    button: {
+      prop$: just({disabled: false, spinner: false, label: 'Sign In'})
+    },
+    http: {
+      url: config.url.login,
+      method: 'POST'
+    }
+  })
 
-  const actions = intent(sources);
-  const {state$, request$} = model({
-    ...actions,
-    http$: sources.HTTP
-  }, initialState$);
-
-  const route$ = delay(1000, state$
-    .filter(state => state.success)
-    .filter(state => state.success.length > 0)
-    .constant('/'))
-
-  const vtree$ = view(state$);
+  const form = HTTPForm(sources, formProp$)
+  const vtree$ = view(form.DOM.map(dom => ({form: dom})))
+  // const initialState$ = just({
+  //   disabled: false,
+  //   errors: [],
+  //   success: [],
+  //   spinner: false
+  // });
+  //
+  // const actions = intent(sources);
+  // const {state$, request$} = model({
+  //   ...actions,
+  //   http$: sources.HTTP
+  // }, initialState$);
+  //
+  // const vtree$ = view(state$);
   return {
     DOM: vtree$,
-    HTTP: request$,
-    router: route$,
+    HTTP: form.HTTP,
   }
 }
 
--- src/pages/login/login-view.js
+++ src/pages/login/login-view.js
@@ -6,66 +6,7 @@ const makeListItem = curry((cls, message) => {
 })
 const view = (state$) => {
   return state$.map(state => {
-    return div('.login', [
-      div('.messages', {style: {top: state.errors.length > 0 ? '15%' : '0'}}, [
-        ul('.message-list', state.errors.map(makeListItem('.error')))
-      ]),
-      div('.messages', {style: {top: state.success.length > 0 ? '15%' : '0'}}, [
-        ul('.message-list', state.success.map(makeListItem('.success')))
-      ]),
-      div('.login-form', [
-        input('.username', {
-          attrs: {
-            type: 'text',
-            placeholder: 'Name',
-            disabled: state.disabled
-          }
-        }),
-        input('.password', {
-          attrs: {
-            type: 'password',
-            placeholder: 'Password',
-            disabled: state.disabled
-          }
-        }),
-        button('.submit', {
-          attrs: {
-            disabled: state.disabled
-          },
-          style: {
-            'border-radius': state.spinner ? '50%' : '3px',
-            'width': state.spinner ? '38px' : '100%',
-            'margin': state.spinner ? '0 auto' : '0 0',
-            'color': state.spinner ? 'transparent' : '#595E5B',
-            'opacity': state.spinner ? '0' : '1',
-          }
-        }, ['Login']),
-        h('svg', {
-          attrs: {
-            class: 'spinner',
-            width: '38px',
-            height: '38px',
-            viewBox: '0 0 39 39',
-            xmlns: "http://www.w3.org/2000/svg"
-          }, style: {
-            'opacity': state.spinner ? '1' : '0',
-            // 'z-index': state.spinner ? '1' : '-1',
-          }
-        },[
-          h('circle', {
-            attrs: {
-              'class': 'path',
-              'fill': 'none',
-              'stroke-width': '6',
-              'stroke-linecap': 'round',
-              'cx': '19',
-              'cy': '19',
-              'r': '16'
-            }
-          })
-        ])
-      ])
-    ])
+    return div('.login', [state.form])
   })
 }
 
